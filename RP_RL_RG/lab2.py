8ba1385d562d80a29f4a7962952d56a451fc1687
Saurabh <sakhanej@cisco.com>
2023-01-17 22:23:12 +0530

    initial commit

---
+++ RP_RL_RG/lab2.py
@@ -0,0 +1,11 @@
+import json
+import os
+from zeep.exceptions import Fault
+import traceback
+import sys
+sys.path.append("../")
+from ciscoaxl import axl
+from common.baseFunctions import *
+from common.importLogic import updateConfigs
+
+### Begin Coding from here for lab 2
